7bcd4dfb0f27b2caa9b8fb77c038694bbd00d8af
Oğuz <132291321+elrondforwin@users.noreply.github.com>
2025-09-04 16:59:10 +0300

    version 1

---
+++ kurulum.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+set -e
+
+iss=""
+
+echo "Bu kurulum dosyasi once Dnscrypt proxy kurulumu yapacak,"
+echo "Ardindan ise byedpictl kurulumu yapip internet servis saglayiciniza gore duzenleyecektir."
+echo
+read -p "Internet servis saglayiciniz superonline mi? (evet/hayir): " cevap
+cevap=$(echo "$cevap" | tr '[:upper:]' '[:lower:]')
+
+while [[ "$cevap" != "evet" && "$cevap" != "e" && "$cevap" != "hayir" && "$cevap" != "h" && "$cevap" != "hayır" ]]; do
+    echo "Lutfen evet ya da hayir olarak cevaplayin (ya da e/h)."
+    read -p "Internet servis saglayiciniz superonline mi? (evet/hayir): " cevap
+    cevap=$(echo "$cevap" | tr '[:upper:]' '[:lower:]')
+done
+
+if [[ "$cevap" =~ ^(evet|e)$ ]]; then
+    iss="superonline"
+    echo "LOG: Kullanici superonline kullaniyor"
+elif [[ "$cevap" =~ ^(hayir|hayır|h)$ ]]; then
+    iss="diger"
+    echo "LOG: Kullanici superonline kullanmiyor"
+fi
+
+while [[ ! -f /bin/dnscrypt-proxy ]]; do
+    echo "Sistemde dnscrypt-proxy tespit edilemedi. Pacman ile kurulum yapiliyor."
+    echo "Lutfen sudo sifresi istenirse girin."
+    echo ""
+    sudo pacman -S dnscrypt-proxy
+    sleep 2
+    echo "dnscrypt-proxy tekrar kontrol ediliyor..."
+    if [[ -f /bin/dnscrypt-proxy ]]; then
+        echo "Kontrol basarili. Devam ediliyor."
+    else
+        echo "Kontrol basarisiz. Tekrar deneniyor..."
+    fi
+done
+
+if [[ -f /bin/dnscrypt-proxy ]]; then
+    echo "dnscrypt-proxy tespit edildi. Devam ediliyor."
+else
+    echo "dnscrypt kurulumunda bir sorun var gibi gorunuyor..."
+    exit 1
+fi
+
+while [[ ! -f /bin/zenity ]]; do
+    echo "Sistemde zenity tespit edilemedi. Pacman ile kurulum yapiliyor."
+    echo "Lutfen sudo sifresi istenirse girin."
+    echo ""
+    sudo pacman -S zenity
+    if [[ -f /bin/zenity ]]; then
+        echo "Kontrol basarili. Devam ediliyor."
+    else
+        echo "Kontrol basarisiz. Tekrar deneniyor..."
+    fi
+done
+
+if [[ -f /bin/zenity ]]; then
+    echo "zenity tespit edildi. Devam ediliyor."
+else
+    echo "zenity kurulumunda bir sorun var gibi gorunuyor..."
+    exit 1
+fi
+
+
+echo "networkmanager dns-none ayarlaniyor..."
+echo "Lutfen sudo sifresi istenirse girin."
+
+sudo cp configs/90-dns-none.conf /etc/NetworkManager/conf.d/90-dns-none.conf
+
+echo "resolv.conf dosyasi guncelleniyor..."
+echo "UYARI: mevcut resolv.conf dosyasinin uzerine yazilacaktir. Orijinal resolv.conf yedekleniyor..."
+
+sudo cp /etc/resolv.conf /etc/resolv.conf.bak
+sudo cp configs/resolv.conf /etc/resolv.conf
+
+echo "dnscrypt-proxy configi kuruluyor..."
+
+sudo cp configs/dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+
+echo "dnscrypt-proxy'i dogru sekilde kullanmak icin masaustu ortaminizin ayarlarindan dns'i 127.0.0.1 olarak ayarlamalisiniz."
+echo "nasil yapilacagini bilmiyorsaniz rehber videosuna bakabilirsiniz."
+
+echo "dnscrypt-proxy konfigure edildi. Servisler baslatiliyor."
+sudo systemctl enable dnscrypt-proxy.service
+sudo systemctl start dnscrypt-proxy.service
+
+echo "dnscrypt-proxdy kurulumu ve konfigrasyonu tamamen tamamlandi. byedpi kurulumuna geciliyor..."
+
+if [ "$iss" = "superonline" ]; then
+    echo "byedpictl superonline kurulum scripti calistiriliyor..."
+    /bin/bash make-superonline.sh
+elif [ "$iss" = "diger" ]; then
+    echo "byedpictl kurulum scripti calistiriliyor..."
+    /bin/bash make.sh
+fi
+
+echo "ByeDPI kuruldu. Sisteminizden byedpictl uygulamasini acarak aktiflestirebilirsiniz."
--- src/bin/byedpictl.sh
+++ src/bin/byedpictl.sh
@@ -9,9 +9,8 @@ PID_DIR="/var/run/$NAME"
 
 CLI_LOG="$LOG_DIR/cli.log"
 
-
-cmd_help () {
-    cat <<EOF
+cmd_help() {
+  cat <<EOF
 $0
 
 COMMANDS:
@@ -25,139 +24,141 @@ COMMANDS:
 EOF
 }
 
-cmd_tun () {
-    case $1 in
-        start)
-            start_tunneling
-            ;;
-        stop)
-            stop_tunneling
-            ;;
-        restart)
-            stop_tunneling
-            start_tunneling
-            ;;
-        status)
-            show_tunneling_status
-            ;;
-        *)
-            echo "Invalid argument!"
-    esac
+cmd_tun() {
+  case $1 in
+  start)
+    start_tunneling
+    ;;
+  stop)
+    stop_tunneling
+    ;;
+  restart)
+    stop_tunneling
+    start_tunneling
+    ;;
+  status)
+    show_tunneling_status
+    ;;
+  *)
+    echo "Invalid argument!"
+    ;;
+  esac
 }
 
-cmd_zenity () {
-    cmd=$(
-        zenity --list --title="$NAME" --hide-header --column="0" \
-        "Tunnel - Start" "Tunnel - Stop" "Tunnel - Restart"
-    )
+cmd_zenity() {
+  cmd=$(
+    zenity --list --title="$NAME" --hide-header --column="0" \
+      "Tunnel - Start" "Tunnel - Stop" "Tunnel - Restart"
+  )
 
-    reply=""
-    case $cmd in
-        "Tunnel - Start")
-            reply=$(pkexec "$0" tun start) || true
-            ;;
-        "Tunnel - Stop")
-            reply=$(pkexec "$0" tun stop) || true
-            ;;
-        "Tunnel - Restart")
-            reply=$(pkexec "$0" tun restart) || true
-            ;;
-    esac
+  reply=""
+  case $cmd in
+  "ByeDPI'i Baslat")
+    reply=$(pkexec "$0" tun start) || true
+    ;;
+  "ByeDPI'i Durdur")
+    reply=$(pkexec "$0" tun stop) || true
+    ;;
+  "ByeDPI'i Yeniden Baslat")
+    reply=$(pkexec "$0" tun restart) || true
+    ;;
+  esac
 
-    zenity --notification --title "$NAME" \
-        --text="$NAME\n$reply"
+  zenity --notification --title "$NAME" \
+    --text="$NAME\n$reply"
 }
 
-prepare_dirs () {
-    mkdir -p "$LOG_DIR"
-    mkdir -p "$PID_DIR"
+prepare_dirs() {
+  mkdir -p "$LOG_DIR"
+  mkdir -p "$PID_DIR"
 }
 
-load_conf () {
-    source "$CONF_DIR/server.conf"
-    source "$CONF_DIR/desync.conf"
+load_conf() {
+  source "$CONF_DIR/server.conf"
+  source "$CONF_DIR/desync.conf"
 }
 
 start_tunneling() {
-    if [[ -f $PID_DIR/tunnel.pid ]]; then
-        echo "Tunnel is already running"
-        exit 1
+  if [[ -f $PID_DIR/tunnel.pid ]]; then
+    echo "Tunnel is already running"
+    exit 1
+  fi
+
+  prepare_dirs
+  load_conf
+
+  ciadpi_args="--ip $CIADPI_IP --port $CIADPI_PORT ${CIADPI_DESYNC[@]}"
+
+  nohup su - byedpi -s /bin/bash -c "$BIN_DIR/ciadpi $ciadpi_args" \
+    >$LOG_DIR/server.log 2>&1 &
+  echo $! >$PID_DIR/server.pid
+  nohup $BIN_DIR/hev-socks5-tunnel $CONF_DIR/hev-socks5-tunnel.yaml \
+    >$LOG_DIR/tunnel.log 2>&1 &
+  echo $! >$PID_DIR/tunnel.pid
+
+  while true; do
+    sleep 0.2
+    if ip tuntap list | grep -q byedpi-tun; then
+      break
     fi
 
-    prepare_dirs
-    load_conf
+    echo "Waiting for tunnel interface..."
+  done
 
+  user_id=$(id -u byedpi)
+  nic_name=$(ip route show to default | awk '$5 != "byedpi-tun" {print $5; exit}')
+  gateway_addr=$(ip route show to default | awk '$5 != "byedpi-tun" {print $3; exit}')
 
-    ciadpi_args="--ip $CIADPI_IP --port $CIADPI_PORT ${CIADPI_DESYNC[@]}"
+  ip rule add uidrange $user_id-$user_id lookup 110 pref 28000
+  ip route add default via $gateway_addr dev $nic_name metric 50 table 110
+  ip route add default via 172.20.0.1 dev byedpi-tun metric 1
 
-    nohup su - byedpi -s /bin/bash -c "$BIN_DIR/ciadpi $ciadpi_args" \
-        > $LOG_DIR/server.log 2>&1 & echo $! > $PID_DIR/server.pid
-    nohup $BIN_DIR/hev-socks5-tunnel $CONF_DIR/hev-socks5-tunnel.yaml \
-        > $LOG_DIR/tunnel.log 2>&1 & echo $! > $PID_DIR/tunnel.pid
-
-   while true; do
-        sleep 0.2
-        if ip tuntap list | grep -q byedpi-tun; then
-            break
-        fi
-
-        echo "Waiting for tunnel interface..."
-    done
-
-    user_id=$(id -u byedpi)
-    nic_name=$(ip route show to default | awk '$5 != "byedpi-tun" {print $5; exit}')
-    gateway_addr=$(ip route show to default | awk '$5 != "byedpi-tun" {print $3; exit}')
-
-    ip rule add uidrange $user_id-$user_id lookup 110 pref 28000
-    ip route add default via $gateway_addr dev $nic_name metric 50 table 110
-    ip route add default via 172.20.0.1 dev byedpi-tun metric 1
-
-    echo "Successfully started the full traffic tunneling"
+  echo "Successfully started the full traffic tunneling"
 }
 
-stop_tunneling () {
-    if [[ ! -f $PID_DIR/tunnel.pid ]]; then
-        echo "Tunnel is not running"
-        exit 1
-    fi
+stop_tunneling() {
+  if [[ ! -f $PID_DIR/tunnel.pid ]]; then
+    echo "Tunnel is not running"
+    exit 1
+  fi
 
-    user_id=$(id -u byedpi)
-    nic_name=$(ip route show to default | awk '$5 != "byedpi-tun" {print $5; exit}')
-    gateway_addr=$(ip route show to default | awk '$5 != "byedpi-tun" {print $3; exit}')
+  user_id=$(id -u byedpi)
+  nic_name=$(ip route show to default | awk '$5 != "byedpi-tun" {print $5; exit}')
+  gateway_addr=$(ip route show to default | awk '$5 != "byedpi-tun" {print $3; exit}')
 
-    ip rule del uidrange $user_id-$user_id lookup 110 pref 28000 2>$CLI_LOG || true
-    ip route del default via "$gateway_addr" dev "$nic_name" metric 50 table 110 2>$CLI_LOG || true
-    ip route del default via 172.20.0.1 dev byedpi-tun metric 1 2>$CLI_LOG || true
+  ip rule del uidrange $user_id-$user_id lookup 110 pref 28000 2>$CLI_LOG || true
+  ip route del default via "$gateway_addr" dev "$nic_name" metric 50 table 110 2>$CLI_LOG || true
+  ip route del default via 172.20.0.1 dev byedpi-tun metric 1 2>$CLI_LOG || true
 
-    kill $(cat $PID_DIR/tunnel.pid) 2>$CLI_LOG || true
-    kill $(cat $PID_DIR/server.pid) 2>$CLI_LOG || true
+  kill $(cat $PID_DIR/tunnel.pid) 2>$CLI_LOG || true
+  kill $(cat $PID_DIR/server.pid) 2>$CLI_LOG || true
 
-    rm -rf "$PID_DIR" || true
+  rm -rf "$PID_DIR" || true
 
-    echo "Successfully stopped the tunneling"
+  echo "Successfully stopped the tunneling"
 }
 
-show_tunneling_status () {
-    server_status="offline"
-    tun_status="offline"
+show_tunneling_status() {
+  server_status="offline"
+  tun_status="offline"
 
-    if [ -f "$PID_DIR/server.pid" ]; then
-        if ps -p $(cat "$PID_DIR/server.pid") > /dev/null 2>&1; then
-            server_status="running"
-        else
-            server_status="crashed"
-        fi
+  if [ -f "$PID_DIR/server.pid" ]; then
+    if ps -p $(cat "$PID_DIR/server.pid") >/dev/null 2>&1; then
+      server_status="running"
+    else
+      server_status="crashed"
     fi
+  fi
 
-    if [ -f "$PID_DIR/tunnel.pid" ]; then
-        if ps -p $(cat "$PID_DIR/tunnel.pid") > /dev/null 2>&1; then
-            tun_status="running"
-        else
-            tun_status="crashed"
-        fi
+  if [ -f "$PID_DIR/tunnel.pid" ]; then
+    if ps -p $(cat "$PID_DIR/tunnel.pid") >/dev/null 2>&1; then
+      tun_status="running"
+    else
+      tun_status="crashed"
     fi
+  fi
 
-    cat <<EOF
+  cat <<EOF
 $NAME background tunneling services
 
 server: $server_status
@@ -166,17 +167,18 @@ EOF
 }
 
 case $1 in
-    help)
-        cmd_help
-        ;;
-    tun)
-        cmd_tun $2
-        ;;
-    zenity)
-        cmd_zenity
-        ;;
-    *)
-        echo "Invalid argument $1"
-        echo "Use \"help\" command."
-        exit 1
+help)
+  cmd_help
+  ;;
+tun)
+  cmd_tun $2
+  ;;
+zenity)
+  cmd_zenity
+  ;;
+*)
+  echo "Invalid argument $1"
+  echo "Use \"help\" command."
+  exit 1
+  ;;
 esac
